07813d63d162835fdb82ff9a22f7d32e185c9f3a
Fitzberth Powell Channer <fitzberth.powellchanner@ucr.ac.cr>
2023-09-12 01:11:07 -0600

    Se cambian palabras por unas mas sencilla y se termina la gramatica inicial

--- calclex.py
+++ calclex.py
@@ -12,68 +12,93 @@ import yacc
 
 # List of token names.   This is always required
 tokens = (
-    #Control - Frases
-    'lasciate ogne i speranza voi chintrate', #FOR
-    'BEATRICCE',
-   'INFERNO', # IF
-   'PURGATORIO', # WHILE
-   'PARADISO', # ELSE
-   'RUBICANTE', # RETURN
-    'MALACODA', # DEF (la wea para hacer metodos)
+    #Control - Frases 
+    'FOR', #'lasciate ogne i speranza voi chintrate'
+    'IF',#'INFERNO' 
+    'WHILE',#'PURGATORIO', 
+    'ELSE',#'PARADISO', 
+    'DEF', #'MALACODA', 
+    'SWITCH', #
+    
+    
     #Variable
-   'DRAGHIGNAZZO', # INT
-   'FARFARELLO', # FLOAT
-   'GRAFFIACANE', # BOOL
-   'LIBICOCCO', # STRING
+    'INT'#'DRAGHIGNAZZO', 
+    'FLOAT'#'FARFARELLO', 
+    'BOOL'#'GRAFFIACANE', 
+    'STRING', #'CIRIATO',
+    'BOOL', # 
    
     #IDs
     'TEXT',
-    'INTEGER', 
-    'BOOL',
-    'FLOAT',
+    'NUMERO', 
+    'REAL',
 
     #Reserved
-   'ALICHINO', # +
-   'BARBARICCIA', # -
-   'CAGNAZZO', # )
-   'CALCABRINA', # (
-   'DANTE', # TRUE
-   'VERGIL', # FALSE
-   'CIRIATO', # STRING
-    'SCARMIGLIONE', # ID
+    'PLUS', # ALICHINO
+    'MINUS', # BARBARICCIA
+    'TIMES', # * ARGENTI
+    'DIVIDE', # BRUTUS
+    'ASSIGN', # BEATRICCE 
+    'RPAREN', #'CAGNAZZO'
+    'LPAREN', #'CALCABRINA'
+    'RBRACKET',#
+    'LBRACKET',#
+    'TRUE', #'DANTE',
+    'FALSE', #'VERGIL',
+    'RETURN', # 'COSA FATTA,CAPPO HA' # RETURN
+    'CASE',#'SCARMIGLIONE' 
+    'BREAK',#
 )
 
-t_ALICHINO    = r'\+'
-t_BARBARICCIA   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_CALCABRINA  = r'\('
-t_CAGNAZZO  = r'\)'
-
-"""
 # Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_FOR = r'LASCIATE OGNE I SPERANZA VOI CHINTRATE'
+t_IF = r'INFERNO'
+t_WHILE = r'PURGATORIO'
+t_ELSE = r'PARADISO'
+t_DEF = r'MALACODA'
+t_SWITCH = r''
 
-"""
+t_INT = r'DRAGHINAZZO'
+t_FLOAT = r'FARFARELLO'
+t_BOOL = r'GRAFFICANE'
+t_STRING = r'CIRIATO'
 
-def t_INTEGER(t):
-    r'\d+'
+t_PLUS    = r'ALICHINO'
+t_MINUS   = r'BARBARICCIA'
+t_TIMES   = r'ARGENTI'
+t_DIVIDE  = r'BRUTUS'
+t_ASSIGN = r'BEATTRICE'
+t_LPAREN  = r'CALCABRINA'
+t_RPAREN  = r'CAGNAZZO'
+t_LBRACKET = r''
+t_RBRACKET = r''
+t_TRUE = r'DANTE'
+t_FALSE = r'VERGIL'
+t_RETURN = r'COSA FATTA,CAPPO HA'
+t_CASE = r'SCARMIGLIONE'
+t_BREAK = r''
+
+def t_NUMERO(t):
+    r'\d+' #numero
     t.value = int(t.value)    
     return t
 
-def t_FLOAT(t):
-    r'\d+'
+def t_REAL(t):
+    r'\d+\.\d+' #numero.numero
     t.value = float(t.value)    
     return t
 
-def t_STRING(t):
-    r'\d+'
-    t.value = str(t.value)    
+def t_ID(t):
+    r'([a-z])'    
+    return t
+
+def t_TEXT(t):
+    r'("[A-Za-z0-9 ,\.]")'
+    t.value = str(t.value)
+    return t
+def t_RETURN(t):
+    r'COSA FATTA,CAPPO HA'
+    t.value = str(t.value)
     return t
 
 
