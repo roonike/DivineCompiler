80a2533d61f73eaa2da28216e6109f0bf9258bbd
Fitzberth Powell Channer <fitzberth.powellchanner@ucr.ac.cr>
2023-11-19 13:17:26 -0600

    AND OR NOT listo

--- ir.py
+++ ir.py
@@ -14,7 +14,7 @@ llvm.initialize_native_asmprinter()
 #enteros de 32 bits
 def int_decl():
     i32 = ir.IntType(32)
-    module = ir.Module(name="module")
+    module = ir.Module(name="INT_Declaration")
     fnty = ir.FunctionType(i32,[])
     func = ir.Function(module,fnty,name= "int_decl")
     block = func.append_basic_block(name="entry")
@@ -26,7 +26,7 @@ def int_decl():
 #Punto flotante de precision simple
 def float_decl():
     f32 = ir.FloatType()
-    module = ir.Module(name="module")
+    module = ir.Module(name="Float_Declaration")
     fnty = ir.FunctionType(f32,[])
     func = ir.Function(module,fnty,name= "float_decl")
     block = func.append_basic_block(name="entry")
@@ -40,7 +40,7 @@ def float_decl():
 def string_decl():
     char = ir.IntType(8)
     string = ir.ArrayType(char,0)
-    module = ir.Module(name="module")
+    module = ir.Module(name="String_Declaration")
     fnty = ir.FunctionType(string,[])
     func = ir.Function(module,fnty,name= "string_decl")
     block = func.append_basic_block(name="entry")
@@ -52,7 +52,7 @@ def string_decl():
 #Entero de 1 bit, 0 = false, 1 = true
 def bool_decl():
     bool = ir.IntType(1)
-    module = ir.Module(name="module")
+    module = ir.Module(name="Bool_Declaration")
     fnty = ir.FunctionType(bool,[])
     func = ir.Function(module,fnty,name= "bool_decl")
     block = func.append_basic_block(name="entry")
@@ -60,6 +60,16 @@ def bool_decl():
     var_type = bool
     builder.alloca(var_type, name="boolVar")
     print(module)
+
+def array_decl(type):
+    if(type == "int"): 
+        arr_type = ir.IntType(32)
+    elif(type == "float"): 
+        arr_type = ir.FloatType()
+    elif(type == "bool"): 
+        arr_type = ir.IntType(1)
+        
+    
 
 def decl():
     int_decl()
@@ -72,7 +82,7 @@ def decl():
 #Asignar valor a entero de 32 bits 
 def int_asign(val):
     i32 = ir.IntType(32)
-    module = ir.Module(name="module")
+    module = ir.Module(name="INT_Asign")
     fnty = ir.FunctionType(i32,[])
     func = ir.Function(module,fnty,name= "int_asign")
     block = func.append_basic_block(name="entry")
@@ -86,7 +96,7 @@ def int_asign(val):
 #asignar valor a flotante
 def float_asign(val):
     f32 = ir.FloatType()
-    module = ir.Module(name="module")
+    module = ir.Module(name="Float_Asign")
     fnty = ir.FunctionType(f32,[])
     func = ir.Function(module,fnty,name= "float_decl")
     block = func.append_basic_block(name="entry")
@@ -104,7 +114,7 @@ def string_asign(val):
     string_arg = []
     for value in val:
         string_arg.append(ord(value))
-    module = ir.Module(name="module")
+    module = ir.Module(name="String_Asign")
     fnty = ir.FunctionType(string,[])
     func = ir.Function(module,fnty,name= "string_decl")
     block = func.append_basic_block(name="entry")
@@ -118,17 +128,14 @@ def string_asign(val):
 #asignar valor a bool
 def bool_asign(val):
     bool = ir.IntType(1)
-    module = ir.Module(name="module")
+    module = ir.Module(name="Bool_Asign")
     fnty = ir.FunctionType(bool,[])
     func = ir.Function(module,fnty,name= "int_asign")
     block = func.append_basic_block(name="entry")
     builder = ir.IRBuilder(block)
     var_type = bool
     bool_var = builder.alloca(var_type, name="boolVar")
-    if(val == True):
-        bool_val = bool(1) #True = 1
-    elif(val == False):
-        bool_val = bool(0) #False = 0
+    bool_val = bool(val)
     builder.store(bool_val,bool_var)
     print(module)
 
@@ -140,7 +147,42 @@ def asign():
 
 
 
+def ir_and(bool1,bool2):
+    bool = ir.IntType(1)
+    module = ir.Module(name="AND_example")
+    fnty = ir.FunctionType(bool,(bool,bool))
+    func = ir.Function(module,fnty,name="And")
+    block = func.append_basic_block(name="entry")
+    builder = ir.IRBuilder(block)
+    resul = builder.and_(bool(bool1),bool(bool2))
+    builder.ret(resul)
+    print(module)
+    
+def ir_or(bool1,bool2):
+    bool = ir.IntType(1)
+    module = ir.Module(name="OR_example")
+    fnty = ir.FunctionType(bool,(bool,bool))
+    func = ir.Function(module,fnty,name="OR")
+    block = func.append_basic_block(name="entry")
+    builder = ir.IRBuilder(block)
+    resul = builder.or_(bool(bool1),bool(bool2))
+    builder.ret(resul)
+    print(module)
 
-decl()    
-asign()
+def ir_not(val):
+    bool = ir.IntType(1)
+    module = ir.Module(name="NOT_example")
+    args = []
+    args.append(bool)
+    fnty = ir.FunctionType(bool,args)
+    func = ir.Function(module,fnty,name="NOT")
+    block = func.append_basic_block(name="entry")
+    builder = ir.IRBuilder(block)
+    resul = builder.not_(bool(val))
+    builder.ret(resul)
+    print(resul)
+    print(module)
+    
+
+ir_not(True)
 
